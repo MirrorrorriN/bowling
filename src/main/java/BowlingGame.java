940940bdcf660d6ab154f80921f0b4dc8c75fd59
MirrorrorriN <somunsfool@163.com>
2017-05-03 19:08:03 +0800

    Update BowlingGame.java

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -5,74 +5,87 @@ public class BowlingGame {
     String[] score=bowlingCode.split("|");;
     int n=score.length;
     int[] all=new int[10];
-    ArrayList ball=new ArrayList();
+    int[] ball=new int[22];
+    int k=0;
     int[] tag={0,0,0,0,0,0,0,0,0,0};
     for(int i=0;i<10;i++){
         int t=0;
         if(score[i].charAt(0)=='X'){
-            ball.add(10); 
+            ball[k]=10;
+            k++;
             all[i]=10;
             tag[i]=2;
             continue;
         }
         if(score[i].charAt(0)=='-'){
-            ball.add(0);
+            ball[k]=0;
+            k++;
         }
         else{
-            ball.add(score[i].charAt(0)-'0');
+            ball[k]=score[i].charAt(0)-'0';
+            k++;
             t+=(score[i].charAt(0)-'0');
         }
         if(score[i].charAt(1)=='/'){
-            ball.add(10-(score[i].charAt(0)-'0'));
+            ball[k]=10-(score[i].charAt(0)-'0');
+            k++;
             all[i]=10;
             tag[i]=1;
         }
         else if(score[i].charAt(1)=='-'){
-            ball.add(0);
+            ball[k]=0;
+            k++;
             all[i]=t;
         }
         else{
-            ball.add(score[i].charAt(1)-'0');
+            ball[k]=score[i].charAt(1)-'0';
+            k++;
             t+=(score[i].charAt(1)-'0');
             all[i]=t;
         }
     }
     if(tag[9]!=0){
         if(score[11].charAt(0)=='X'){
-            ball.add(10);
+            ball[k]=10;
+            k++;
         }
         else if(score[11].charAt(0)=='-'){
-            ball.add(0);
+            ball[k]=0;
+            k++;
         }
         else{
-            ball.add(score[11].charAt(0)-'0');
+            ball[k]=score[11].charAt(0)-'0';
+            k++;
         }
         if(tag[9]==2){
             if(score[11].charAt(1)=='X'){
-                ball.add(10);
+                ball[k]=10;
+                k++;
             }
             else if(score[11].charAt(1)=='/'){
-                ball.add(10-score[11].charAt(0)+'0');
+                ball[k]=10-score[11].charAt(0)+'0';
+                k++;
             }
             else if(score[11].charAt(1)=='-'){
-                ball.add(0);
+                ball[k]=0;
+                k++;
             }
             else{
-                ball.add(score[11].charAt(1)-'0');
+                ball[k]=score[11].charAt(1)-'0';
+                k++;
             }
         }
     }
-    int[] nball=(int[])ball.toArray(new int[ball.size()]);
     int res=0;
     int index=0;
     for(int i=0;i<10;i++){
         if(tag[i]==2){
             index+=1;
-            res+=(all[i]+nball[index]+nball[index+1]);
+            res+=(all[i]+ball[index]+ball[index+1]);
         }
         else if(tag[i]==1){
             index+=2;
-            res+=(all[i]+nball[index]);
+            res+=(all[i]+ball[index]);
         }
         else{
             index+=2;
